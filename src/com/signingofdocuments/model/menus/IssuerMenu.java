cd5a902f8459b27203a764ac732c6716b495d20c
Edmilson Carvalho <edcarvalho.dev@gmail.com>
2025-07-25 12:08:15 -0300

    feat: adicionadas classes de menu para assinante e emissor
    Criadas classes SignatoryMenu e IssuerMenu que herdam da classe Menu abstrata e encapsulam a definição dos menus para cada tipo de usuário

---
+++ src/com/signingofdocuments/model/menus/IssuerMenu.java
@@ -0,0 +1,9 @@
+package com.signingofdocuments.model.menus;
+
+public class IssuerMenu extends Menu {
+    public IssuerMenu() {
+        menuInfo = "MENU DE EMISSOR" +
+                "\n1. Emitir documento" +
+                "\n2. Sair";
+    }
+}
---
+++ src/com/signingofdocuments/model/menus/SignatoryMenu.java
@@ -0,0 +1,9 @@
+package com.signingofdocuments.model.menus;
+
+public class SignatoryMenu extends Menu {
+    public SignatoryMenu() {
+        menuInfo = "MENU DE ASSINANTE" +
+                "\n1. Assinar documento" +
+                "\n2. Sair";
+    }
+}
